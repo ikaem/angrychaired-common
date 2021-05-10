8ccb8d1ca9e8d41befc800d1bfa520b825de5ac2
Karlo Marinovic <karlo.marinovic@init.hr>
2021-05-10 20:53:04 +0200

    additonal config

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// common\src\index.ts
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 10,
+  green: 10,
+};
+
+console.log('color', color);
+
+export default color;
